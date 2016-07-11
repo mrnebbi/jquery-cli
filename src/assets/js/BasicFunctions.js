3e5c2d164e8d4434c0a845af973748087ffa8ac1
gavbarnett <gavbarnett@gmail.com>
2016-07-11 21:32:00 +0100

    Edited some switch defaults to retun a basic error msg. Also help function time [time help]

--- src/assets/js/BasicFunctions.js
+++ src/assets/js/BasicFunctions.js
@@ -25,8 +25,11 @@ var BasicFunctions = {
       //show licence agreement
         stdout = '<em>The MIT License (MIT)<br /><br />Copyright (c) 2016 Ian Isted<br />Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br />The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</em>';
         break;
-      default:
+      case "":
         stdout = '<em>CLI version 1.0.<br />Built by <a href="http://twitter.com/ianisted" target="_blank">@ianisted</a>.<br />Released under <a href="https://github.com/ianisted/jquery-cli/blob/master/LICENSE" target="_blank">MIT license</a>. Type <strong>about license</strong> to see the license.</strong></em>';
+        break;
+      default:
+        stdout = "ERROR: This is not a valid command. Please see 'About Help'"
         break;
     }
     return stdout;
@@ -113,6 +116,9 @@ var BasicFunctions = {
     var stdout = "";
     var d = new Date();
     switch(stdin.toLowerCase()) {
+      case "help":
+        stdout = '<br />HELP: TIME []<br />' + '<br  />  TIME : Displays current time in UK format<br />  TIME [STAMP] : Displays current unix time<br />  TIME [DATE] : Displays current date in UK format<br />  TIME [FULL] : Displays current time & date in UK format';
+        break;
       case "stamp":
       //returns unix time (milliseconds since midnight on 1st Jan 1970)
         stdout = "" + d.getTime();
@@ -130,7 +136,7 @@ var BasicFunctions = {
         stdout = "" + d.toLocaleString("en-GB");
         break;
       default:
-        stdout = "" + d.getTime();
+        stdout = "ERROR: This is not a valid command. Please see 'Time Help'"
         break;
     }
     return stdout;
