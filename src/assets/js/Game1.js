69208dfe81f01ffe3aee969234900e1ca3f29bdd
gavbarnett <gavbarnett@gmail.com>
2016-07-20 21:32:42 +0100

    Added Look [] function

--- src/assets/js/Game1.js
+++ src/assets/js/Game1.js
@@ -11,7 +11,7 @@ var Game1 = {
   Please keep in alphabetical order for my OCD :-D
 */
   go_direction:function(stdin){
-    /*demo comments;
+    /*go is used to move around the world;
     */
 
     //required variable
@@ -39,24 +39,24 @@ var Game1 = {
         }
       },
       'south': function(stdin) {
-        //travel north
+        //travel south
         if (stdin != "function_menu"){
           current_location.y -= 1;
-          stdout = "You travel North. Current Location: " + current_location["x"] + ", " + current_location["y"];
+          stdout = "You travel South. Current Location: " + current_location["x"] + ", " + current_location["y"];
         }
       },
       'east': function(stdin) {
-        //travel north
+        //travel east
         if (stdin != "function_menu"){
           current_location.x += 1;
-          stdout = "You travel North. Current Location: " + current_location["x"] + ", " + current_location["y"];
+          stdout = "You travel East. Current Location: " + current_location["x"] + ", " + current_location["y"];
         }
       },
       'west': function(stdin) {
-        //travel north
+        //travel west
         if (stdin != "function_menu"){
           current_location.x -= 1;
-          stdout = "You travel North. Current Location: " + current_location["x"] + ", " + current_location["y"];
+          stdout = "You travel West. Current Location: " + current_location["x"] + ", " + current_location["y"];
         }
       },
       '': function(stdin) {
@@ -90,6 +90,83 @@ var Game1 = {
     return (stdout);
 
   },
+
+  look:function(stdin){
+    /*look is used to examine the area or objects
+    */
+
+    //required variable
+    var stdout = "";
+    var function_menu = "";
+
+    //function variables
+
+    //function_menu structure
+    function_menu = {
+      'default': function(stdin) { //required option
+        //default option
+        stdout = "ERROR: This is not a valid command. Please see 'Go Help'";
+      },
+      'help': function(stdin) { //required option
+        //big help statement
+        stdout = '<br />HELP: LOOK []<br />' + "<br  />  LOOK: Look around local area. <br /> LOOK [OBJECT]: Look at or examine an object. <br /> LOOK [DIRECTION]: Look in a general direction. <br />";
+      },
+      /* function specific options */
+      'north': function(stdin) {
+        //look  north
+        if (stdin != "function_menu"){
+          stdout = "Looking North you see ...";
+        }
+      },
+      'south': function(stdin) {
+        //look south
+        if (stdin != "function_menu"){
+          stdout = "Looking South you see ...";
+        }
+      },
+      'east': function(stdin) {
+        //look east
+        if (stdin != "function_menu"){
+          stdout = "Looking East you see ...";
+        }
+      },
+      'west': function(stdin) {
+        //look west
+        if (stdin != "function_menu"){
+          stdout = "Looking West you see ...";
+        }
+      },
+      '': function(stdin) {
+        //display general about information
+        stdout = "You are in a [Blank]. You see some [blank]";
+      }
+    };
+
+    //Additional 'hidden menu' options
+    switch (stdin){
+      case 'function_menu': //required option
+        //sends internal function_menu out of stdout - used for tabcomplete
+        stdout = function_menu;
+        return (stdout);
+        break;
+      case 'helpdir': //required option
+        //oneliner help statement
+        stdout = "<strong>look</strong>: look and examine the world around you.";
+        return (stdout);
+        break;
+      default:
+        //default is to look up function_menu
+        (function_menu[stdin]|| function_menu['default'])(stdin);
+        break;
+    }
+
+    //normally required echo of stdout
+    BasicFunctions.echo(stdout);
+
+    //required return of stdout
+    return (stdout);
+  },
+
   init_menu:function(stdin){
     /*Adds the BasicFunctions options to the
       command line menu.
@@ -106,6 +183,9 @@ var Game1 = {
       /* function specific options */
       'go': function(stdin) {
         return Game1.go_direction(stdin);
+      },
+      'look': function(stdin) {
+        return Game1.look(stdin);
       }
     };
 
